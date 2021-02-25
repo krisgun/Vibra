f31cf98151a4368ad7cc0af9d959943d625cf464
Kristoffer Gunnarsson <krigun@kth.se>
2021-02-26 00:57:13 +0100

    Started on input check on measuring time

--- app/src/main/java/com/krisgun/vibra/ui/measure/MeasureViewModel.kt
+++ app/src/main/java/com/krisgun/vibra/ui/measure/MeasureViewModel.kt
@@ -7,20 +7,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.krisgun.vibra.R
+import com.krisgun.vibra.util.ObservableViewModel
 
 private const val TAG = "MeasureViewModel"
 
-class MeasureViewModel : ViewModel() {
+class MeasureViewModel : ObservableViewModel() {
 
     private lateinit var navController: NavController
 
-    private val _durationMinutes = MutableLiveData<Int>()
-    val durationMinutes: LiveData<Int>
+    private val _durationMinutes = MutableLiveData<String>()
+    val durationMinutes: LiveData<String>
         get() = _durationMinutes
 
-    private val _durationSeconds = MutableLiveData<Int>()
-    val durationSeconds: LiveData<Int>
-        get() = _durationMinutes
+    private val _durationSeconds = MutableLiveData<String>()
+    val durationSeconds: LiveData<String>
+        get() = _durationSeconds
+
+    private val _countdownSeconds = MutableLiveData<String>()
+    val countdownSeconds: LiveData<String>
+        get() = _countdownSeconds
+
+    init {
+        _durationMinutes.value = "01"
+        _durationSeconds.value = "30"
+        _countdownSeconds.value = "05"
+    }
 
     fun setNavController(navController: NavController) {
         this.navController = navController
@@ -32,7 +43,29 @@ class MeasureViewModel : ViewModel() {
         navController.navigate(action)
     }
 
-    fun onTextChangedDurationMinuteText() {
-        Log.d(TAG, "Clicky duration minute text!")
+    /**
+     * TODO: IMPLEMENT AFTER TEXT CHANGED. SINGLE DIGIT SHOULD BE PREPENDED WITH A 0. EX 1 -> 01
+     */
+
+    fun onTextChangedDurationMinuteText(text: CharSequence) {
+        onTextChangedSetTimeText(text, _durationMinutes)
+    }
+
+    fun onTextChangedDurationSecondsText(text: CharSequence) {
+        onTextChangedSetTimeText(text, _durationSeconds)
+    }
+
+    fun onTextChangedCountdownSecondsText(text: CharSequence) {
+        onTextChangedSetTimeText(text, _countdownSeconds)
+    }
+
+    private fun onTextChangedSetTimeText(text: CharSequence, data: MutableLiveData<String>) {
+        if (text.isNotEmpty()) {
+            val textToChange = text.toString().toInt()
+            if (textToChange in 6..9) {
+                textToChange.toString()
+                data.value = "0$textToChange"
+            }
+        }
     }
 }
