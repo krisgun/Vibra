58fb5bf9571466900968c1b35f81d227ffc2fa90
Kristoffer Gunnarsson <krigun@kth.se>
2021-03-22 17:21:28 +0100

    Fix discard measurement navigation bug

--- app/src/main/java/com/krisgun/vibra/ui/measure/MeasureFragment.kt
+++ app/src/main/java/com/krisgun/vibra/ui/measure/MeasureFragment.kt
@@ -5,7 +5,6 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navGraphViewModels
@@ -24,6 +23,7 @@ class MeasureFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         navController = findNavController()
         setHasOptionsMenu(true)
     }
